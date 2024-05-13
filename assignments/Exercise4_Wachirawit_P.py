ab870d176370128288d2c817c62b748a6aac4129
FillkungX <weirxvvxez@gmail.com>
2024-05-13 22:13:11 +0700

    score

---
+++ assignments/Exercise4_Wachirawit_P.py
@@ -0,0 +1,11 @@
+fe = float(input())
+gb = float(input())
+ics = float(input())
+cp = float(input())
+
+#Score Output
+print("--- Your Score ---")
+print("Foundation English : ",fe)
+print("General Business : ",gb)
+print("Introduction to Computer Systems : ",ics)
+print("Computer Programming : ",cp)
